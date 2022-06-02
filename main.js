4b69f9505a20abd90a61776932bed3789561823c
Helena Back <helena.back@student.uibk.ac.at>
2022-06-02 18:16:17 +0200

    Vorhersaezeitpunkt als Tooltip anzeigen

--- main.js
+++ main.js
@@ -114,8 +114,11 @@ let popup = `
 //Wettericon(s)
 for (let i=0; i <= 24; i+=3){
     let symbol = jsondata.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
-popup += `<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px">`;
-}
+    let forecastDate = new Date(jsondata.properties.timeseries[i].time);
+    let forecastLabel = formatDate(forecastDate);
+
+    popup += `<img src="icons/${symbol}.svg"  title ="${forecastLabel}" alt="${symbol}" style="width:32px">`;
+};
 
 
 marker.setPopupContent(popup).openPopup();
